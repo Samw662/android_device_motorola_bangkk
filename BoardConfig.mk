a9963cf43fc2cfd368c6833dc531437e0c739133
BarryBlackCat <silva.tiago0910@gmail.com>
2024-06-23 00:03:51 -0300

    bangkk: we are using prebuilt kernel for now
    Signed-off-by: BarryBlackCat <silva.tiago0910@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 #Stock use gzip let's try
 #BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc LLVM=1
-TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
+#TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
 #TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig
 
 BOARD_KERNEL_BINARIES := kernel
